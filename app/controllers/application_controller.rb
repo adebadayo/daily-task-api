950dd186b4f3bbdf6d4ffa9080df259b4b8432a3
adebadayo <adebayanen@gmail.com>
2022-01-08 17:53:17 +0900

    google omniauth で必要なファイル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if current_user
+
+    # redirect_to root_path
+  end
+
+  def current_user
+    return unless (user_id = session[:user_id])
+
+    @current_user ||= User.find_by(id: user_id)
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
